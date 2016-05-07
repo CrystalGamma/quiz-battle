bec837122e495b82c503480ce158206b478374e4
Jona Stubbe (CrystalGamma) <dev@jona-stubbe.de>
2016-05-07 15:59:21 +0200

    +login test page

--- public/static/login.js
+++ public/static/login.js
@@ -1,27 +1,36 @@
-(() => {
+(() => {"use strict";
 const loginBoxes = document.querySelectorAll('.login');
 if (!localStorage) {return}
 let login = localStorage.getItem('login');
 if (!login) {
 	for (let el of loginBoxes) {
-		el.appendChild(buildDom({'':'form.inline', c:[
-			{'':'label', c:{'':'input', name:'user', placeholder:"Username"}},
-			{'':'label', c:{'':'input', name:'password', type:'password', placeholder:"Passwort", autocomplete:'current-password'}},
+		el.appendChild(buildDom({'':'form', c:[
+			{'':'input', name:'user', placeholder:"Username"},
+			{'':'input', name:'password', type:'password', placeholder:"Passwort", autocomplete:'current-password'},
 			{'':'button', type:'submit', c: "Login"}
 		]}));
 	}
-	console.log('listener');
 	document.body.addEventListener('submit', ev => {
-		console.log('test');
-		if (!ev.target.parentNode.classList.contains('login')) {return}
-		localStorage.setItem('login', JSON.stringify({username: ev.target.user.value}));
-		location.reload();
+		const container = ev.target.parentNode;
+		if (!container.classList.contains('login')) {return}
 		ev.preventDefault();
+		makeXHR('POST', container.dataset.auth, {'Content-Type':'application/json'}, xhr => {
+			if (xhr.status === 200 && xhr.getResponseHeader('Content-Type') === 'text/plain; charset=UTF-8') {
+				localStorage.setItem('login', JSON.stringify({user: ev.target.user.value}));
+				location.reload();
+			} else if (xhr.status >= 200 && xhr.status <300) {
+				alert(`Interner Fehler: Unerwartetes Anmeldetoken-Format: Status ${xhr.status}, ${xhr.getResponseHeader('Content-Type')}`);
+			} else if (xhr.status === 404) {
+				alert("Kein Account mit diesem Namen vorhanden");
+			} else {
+				alert(`Anmeldung fehlgeschlagen (${xhr.statusText}): ${xhr.body}`);
+			}
+		}).send(JSON.stringify({user:ev.target.user.value, password: ev.target.password}));
 	});
 } else {
 	login = JSON.parse(login);
 	for (let el of loginBoxes) {
-		el.appendChild(buildDom({'':'p', c:["Hallo, ", {'':'a.user', c: login.username}, {'':'button.logout', type:'button', c:"Logout"}]}));
+		el.appendChild(buildDom({'':'p', c:["Hallo, ", {'':'a.user', c: login.user}, {'':'button.logout', type:'button', c:"Logout"}]}));
 	}
 	document.addEventListener('click', ev => {
 		if (!ev.target.classList.contains('logout')) {return}
