4cfec3ea5e778263a642697c019816912fec33da
Jona Stubbe (CrystalGamma) <dev@jona-stubbe.de>
2016-05-08 13:22:14 +0200

    schema: fix up, works on postgresql now

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 create table kategorie(
 	id serial primary key not null,
-	name varchar(255) not null,
+	name varchar(255) not null
 );
 
 create table frage(
@@ -10,7 +10,7 @@ create table frage(
 	falsch1 varchar(50) not null,
 	falsch2 varchar(50) not null,
 	falsch3 varchar(50) not null,
-	bild blob null,
+	bild bytea null,
 	erklaerung varchar(255) not null	-- nicht vorhanden, wenn leerer string
 );
 
@@ -34,7 +34,7 @@ create table spiel(
 	runden int not null,
 	fragen_pro_runde int not null,
 	fragenzeit int not null,
-	rundenzeit int not null,
+	rundenzeit int not null
 );
 
 create table teilnahme(
@@ -47,7 +47,7 @@ create table teilnahme(
 create table runde(
 	spiel int references spiel(id) not null,
 	rundennr int not null,
-	start datetime not null,
+	start timestamp not null,
 	dealer int references spieler(id) not null,
 	kategorie int references kategorie(id) null,
 	primary key (spiel, rundennr)
@@ -66,7 +66,7 @@ create table antwort(
 	spieler int references spieler(id) not null,
 	fragennr int not null,
 	antwort int null,
-	startzeit datetime not null,
+	startzeit timestamp not null,
 	foreign key (spiel, fragennr) references spiel_frage(spiel, fragennr),
 	foreign key (spiel, spieler) references teilnahme(spiel, spieler),
 	primary key (spiel, spieler, fragennr)
