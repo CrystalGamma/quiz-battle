5c7a7746ac2ddcdfe92e4dc682e18a29be21b362
Florian Bussmann <hallo@florian-bussmann.de>
2016-05-09 12:34:39 +0200

    utf8 + json questions

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new PDO('mysql:host=localhost;dbname=quizduell',"root", "");
+$conn = new PDO('mysql:host=localhost;dbname=quizduell',"root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") );
 if(!$conn){
     http_response_code(500);
     die("Datenbank nicht erreichbar");
