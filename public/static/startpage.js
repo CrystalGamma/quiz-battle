ef39ffd3cadac36b22d85367b0217272016fda66
Jona Stubbe <dev@jona-stubbe.de>
2016-05-19 21:50:29 +0200

    /: show correct score

--- public/static/startpage.js
+++ public/static/startpage.js
@@ -1,18 +1,20 @@
 (()=>{"use strict";// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 	const newGameUrl = document.currentScript.dataset.newgame;
 	const $main = document.querySelector('main');
-	loginPromise.then(login => {
+	loginPromise.then(login => makeXHR('GET', login.player_||login.player[''], {Accept: 'application/json', Authorization: login.token}, xhr => {
+		if (xhr.status < 200 || xhr.status >= 300 || xhr.getResponseHeader('Content-Type') !== 'application/json') {return}
+		const json = JSON.parse(xhr.responseText);
 		$main.appendChild(buildDom({'':'section#status', c:[
 			{'':'h1', c:login.user},
 			{c:{'':'ul.stat', c:[
 				{'':'li', c:["Ranking: ", {'':'span.figure', c: '1234'}]},
-				{'':'li', c:["Punkte: ", {'':'span.figure', c: '123'}]}
+				{'':'li', c:["Punkte: ", {'':'span.figure', c: ''+json.score}]}
 			]}},
 			{'':'a.start-game', href: newGameUrl, c: "Neues Spiel"}
 		]}));
-		makeXHR('GET', login.player_||login.player[''], {Accept: 'application/json', Authorization: login.token}, xhr => xhr.status >= 200 && xhr.status < 300 && xhr.getResponseHeader('Content-Type') === 'application/json' ? buildDom([
-			{'':'section#open-games', c:[{'':'h1', c:"Spiele gegen …"}, {'':'ul', c:JSON.parse(xhr.responseText).activegames_.map(showGame(login))}]},
+		buildDom([
+			{'':'section#open-games', c:[{'':'h1', c:"Spiele gegen …"}, {'':'ul', c:json.activegames_.map(showGame(login))}]},
 			{'':'section#waiting-games', c:[{'':'h1', c:"Warten auf …"}]},
-		]).forEach(x => $main.appendChild(x)) : console.error(xhr)).send();
-	});
+		]).forEach(x => $main.appendChild(x));
+	}).send());
 })();
