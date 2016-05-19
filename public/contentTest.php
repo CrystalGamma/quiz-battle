0eba7fcc548c8be512122cbd4005d3b844ce924f
Jona Stubbe <dev@jona-stubbe.de>
2016-05-19 12:09:46 +0200

    -contentTest.php

--- public/contentTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once "../classes/ContentNegotation.php";
-    $header=$_SERVER['HTTP_ACCEPT'];
-    $server="text/html,application/json;q=0.9";
-    $content=ContentNegotation::getContent($header,$server);
-    print_r($content);
-?>
