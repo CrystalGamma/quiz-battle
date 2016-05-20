11500e01dbcbd010f0ad470d5439cb152a4096e0
hwgfp <westhof.games@gmail.com>
2016-05-20 08:56:28 +0200

    kathegoriename ins JSON aufgenommen und schnellfix

--- public/scripts/game.php
+++ public/scripts/game.php
@@ -7,7 +7,7 @@
 		{"": "/players/<id>", "name": "Spielername", "accepted": true}
 	],
 	"rounds": [{
-		"category_": "/categories/<id>",
+		"category_": {"":"/categories/<id>","name":"name"},
 		"dealer": {"": "/players/<id>", "name": "Spielername"},
 		"started": "2016-05-08T10:33:52Z"
 	}, {
@@ -75,39 +75,40 @@ if($request=='GET'){
 }
     
 }else if($_SERVER['REQUEST_METHOD']=='PUT'){
-	$inputJSON = file_get_contents('php://input');
-	$input= json_decode( $inputJSON, TRUE ); //convert JSON into array
-	if ($input[''] !== '/schema/response') {
-		http_response_code(400);
-		die('Falsches Datenformat');
-	}
-	$username = getAuthorizationUser();
-	if ($username === false) {
-		http_response_code(401);
-		header('WWW-Authenticate: Token');
-		die('Zum Annehmen oder Ablehnen von Spielen muss ein gültiger Authentifikationstoken vorliegen');
-	}
-	if ($input['accept'] === true) {
-		$stmt = $conn->prepare('UPDATE teilnahme t, spieler s SET t.akzeptiert=1 WHERE t.spieler=s.id AND spiel=:spiel AND s.name = :spieler');
-		if (!$stmt->execute(['spiel' => $anzuzeigendesSpielID, 'spieler' => $username])) {
-			http_response_code(500);
-			var_dump($stmt->errorInfo());
-			die();
-		}
-	} else {
-		// TODO: should games only be deleted if there are less than 2 participants?
-		$stmt = $conn->prepare('DELETE FROM spiel where id = ?');
-		if (!$stmt->execute([$anzuzeigendesSpielID])) {
-			http_response_code(500);
-			var_dump($stmt->errorInfo());
-			die();
-		}
-	}
-	if (!$conn->commit()) {
-		http_response_code(500);
-		header('Retry-After: 3');
-		die('Transaktion gescheitert');
-	}
+       $inputJSON = file_get_contents('php://input');
+       $input= json_decode( $inputJSON, TRUE ); //convert JSON into array
+       if ($input[''] !== '/schema/response') {
+               http_response_code(400);
+               die('Falsches Datenformat');
+       }
+       $username = getAuthorizationUser();
+       if ($username === false) {
+               http_response_code(401);
+               header('WWW-Authenticate: Token');
+               die('Zum Annehmen oder Ablehnen von Spielen muss ein gültiger Authentifikationstoken vorliegen');
+       }
+       if ($input['accept'] === true) {
+               $stmt = $conn->prepare('UPDATE teilnahme t, spieler s SET t.akzeptiert=1 WHERE t.spieler=s.id AND spiel=:spiel AND s.name = :spieler');
+               if (!$stmt->execute(['spiel' => $anzuzeigendesSpielID, 'spieler' => $username])) {
+                       http_response_code(500);
+                       var_dump($stmt->errorInfo());
+                       die();
+               }
+       } else {
+               // TODO: should games only be deleted if there are less than 2 participants?
+               $stmt = $conn->prepare('DELETE FROM spiel where id = ?');
+               if (!$stmt->execute([$anzuzeigendesSpielID])) {
+                       http_response_code(500);
+                       var_dump($stmt->errorInfo());
+                       die();
+               }
+       }
+       if (!$conn->commit()) {
+               http_response_code(500);
+               header('Retry-After: 3');
+               die('Transaktion gescheitert');
+       }
+//require_once  __DIR__."/answerRequest.php";
 }else if($_SERVER['REQUEST_METHOD']=='POST'){
     $inputJSON = file_get_contents('php://input');
     $input= json_decode( $inputJSON, TRUE ); //convert JSON into array
@@ -121,6 +122,7 @@ if($request=='GET'){
         var_dump($stmt->errorInfo());
         die();
     }
+//require_once  __DIR__."/chooseRequest.php";
 }else{
     http_response_code(405);
     die();
@@ -130,6 +132,7 @@ function getGame (){
         //Players
         global $conn;
         global $anzuzeigendesSpielID;
+        global $nutzername;
         $contentType=ContentNegotation::getContent($_SERVER['HTTP_ACCEPT'],"text/html,application/json;q=0.9");
         $stmt= $conn->prepare('select spieler.name, spieler.id, teilnahme.akzeptiert from spieler, teilnahme where spieler.id=teilnahme.spieler and teilnahme.spiel=? order by spieler.id;');
         $stmt->execute([$anzuzeigendesSpielID]);
@@ -143,16 +146,26 @@ function getGame (){
                 ]
             );
         };
-        $stmt= $conn->prepare('select runde.rundennr, runde.kategorie, spieler.id, spieler.name, runde.start from runde, kategorie, spieler where runde.dealer=spieler.id and runde.spiel=?  group by runde.rundennr order by runde.rundennr;');
+        //bestimme die Stelle des angemeldeten Nutzers in dem Array
+        $playerPos;
+        foreach ($spieler as $key => $value) {
+            if($value['name']===$nutzername) {
+                $playerPos=$key;
+            }
+        }
+        $stmt= $conn->prepare('select runde.rundennr, runde.kategorie as kategorieID, kategorie.name as kategorieName, spieler.id as dealerID, spieler.name as dealerName, runde.start from runde, kategorie, spieler where runde.dealer=spieler.id and runde.kategorie=kategorie.id and runde.spiel=? group by runde.rundennr order by runde.rundennr;');
         $stmt->execute([$anzuzeigendesSpielID]);
         $runden=array();
         foreach ($stmt->fetchall() as $value){
             array_push($runden,
                 [
-                "category_"=> "/categories/".$value['kategorie'],
+                "category_"=>[
+                ""=>"/categories/".$value['kategorieID'],
+                "name" =>$value['kategorieName']
+                ],
             "dealer"=> [
-                        ""=> "/players/".$value['id'],
-                        "name"=>$value['name']
+                        ""=> "/players/".$value['dealerID'],
+                        "name"=>$value['dealerName']
                         ],
             "started"=> date(DATE_ISO8601, strtotime($value['start']))
                 ]
@@ -161,7 +174,9 @@ function getGame (){
         $stmt= $conn->prepare('select spiel.runden, spiel.fragenzeit, spiel.rundenzeit, (case when spiel.dealer=NULL then "firstanswer" else spiel.dealer end) as dealingrule from spiel where spiel.id=?');
         $stmt->execute([$anzuzeigendesSpielID]);
         $spiel=$stmt->fetchall();
-        $stmt= $conn->prepare('select spiel_frage.fragennr,teilnahme.spieler, (case when antwort.startzeit+spiel.fragenzeit < now() then "" else antwort.antwort end) as antwort from (spiel, teilnahme, spiel_frage) left join antwort on (spiel_frage.fragennr=antwort.fragennr and antwort.spiel=spiel_frage.spiel and teilnahme.spieler=antwort.spieler) where spiel_frage.spiel=? and teilnahme.spiel=spiel_frage.spiel and teilnahme.spiel=spiel.id order by spiel_frage.fragennr, teilnahme.spieler');
+        $stmt= $conn->prepare('select spiel_frage.fragennr,teilnahme.spieler, (case when antwort.startzeit+spiel.fragenzeit < now() then "" else antwort.antwort end) as antwort, (case when antwort.startzeit+spiel.fragenzeit < now() then "abgel" else antwort.antwort end) as status from (spiel, teilnahme, spiel_frage) left join antwort on (spiel_frage.fragennr=antwort.fragennr and antwort.spiel=spiel_frage.spiel and teilnahme.spieler=antwort.spieler) where spiel_frage.spiel=? and teilnahme.spiel=spiel_frage.spiel and teilnahme.spiel=spiel.id order by spiel_frage.fragennr, teilnahme.spieler;
+
+');
         $stmt->execute([$anzuzeigendesSpielID]);
         $RueckgabeWert=$stmt->fetchall();
         $fragen = [];
@@ -170,23 +185,41 @@ function getGame (){
             $fragenID=$RueckgabeWert[0]['fragennr'];
             foreach ($RueckgabeWert as $value) {
                     if ($value['fragennr'] === $fragenID) {
+                            if($value['antwort']==="" or $value['antwort']===null ){
                             array_push($tmp, $value['antwort']);
+                            }else{
+                            array_push($tmp, (int)$value['antwort']);
+                            }
+                            if($value['status']===null){
+                                $tmp=null;
+                            }
                     } else {
                             array_push($fragen, [
-                                    "" => $fragenID,
+                                    "" => (int)$fragenID,
                                     "answers" => $tmp
                             ]);
                             $fragenID=$value['fragennr'];
                             $tmp=array();
+                            if($value['antwort']==="" or $value['antwort']===null ){
                             array_push($tmp, $value['antwort']);
+                            }else{
+                            array_push($tmp, (int)$value['antwort']);
+                            }
+                            if($value['status']===null){
+                                $tmp=null;
+                            }
+                            if($value['status']===null){
+                                $tmp=null;
+                            }
                     };
             }
             array_push($fragen, [
-                    "" => $fragenID,
+                    "" => (int)$fragenID,
                     "answers" => $tmp
             ]);
         }
-        $stmt= $conn->prepare('select (spiel.runden*spiel.fragen_pro_runde) as gesAnzFragen, count(teilnahme.spieler) as anzSpieler from spiel, teilnahme where teilnahme.spiel=spiel.id and teilnahme.akzeptiert=true and spiel.id=?;');
+        //Noch nciht beantwortete Fragen werden mit NULL belegt
+        $stmt= $conn->prepare('select spiel.runden, spiel.fragen_pro_runde,(spiel.runden*spiel.fragen_pro_runde) as gesAnzFragen, count(teilnahme.spieler) as anzSpieler from spiel, teilnahme where teilnahme.spiel=spiel.id and teilnahme.akzeptiert=true and spiel.id=1;');
         $stmt->execute([$anzuzeigendesSpielID]);
         $RueckgabeDaten=$stmt->fetchall();
         $anzVorhandenerFragen=count($fragen);
