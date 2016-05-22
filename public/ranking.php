26e3873a8fd7902214e688e8cbd274be50f81457
Florian Bussmann <hallo@florian-bussmann.de>
2016-05-22 18:53:59 +0200

    fix last entry in ranking board

--- public/ranking.php
+++ public/ranking.php
@@ -6,10 +6,10 @@ header('Content-Type: text/html; charset=UTF-8');
 <main>
 <h1>Rangliste</h1>
 <div>
-<?php if($array['prev_']) {?><a rel=prev href="<?=htmlspecialchars($array['prev_'], ENT_QUOTES, 'utf-8')?>">Vorherige</a><?php } ?>
+<?php if ($array['prev_']) {?><a rel=prev href="<?=htmlspecialchars($array['prev_'], ENT_QUOTES, 'utf-8')?>">Vorherige</a><?php } ?>
 <table>
 <thead><tr><th>Rang<th>Name<th>Punkte</thead>
-<tbody><?php foreach($array['players'] as $idx => $player) { ?><tr><td><?=$player['ranking']?>.<th><a class=player href="<?=$player['']?>"><?=$player['name']?><td><?=$player['points']?><?php } ?></tbody>
+<tbody><?php foreach($array['players'] as $idx => &$player) { ?><tr><td><?=$player['ranking']?>.<th><a class=player href="<?=$player['']?>"><?=$player['name']?><td><?=$player['points']?><?php } ?></tbody>
 </table>
 <?php if ($array['next_']) {?><a rel=next href="<?=htmlspecialchars($array['next_'], ENT_QUOTES, 'utf-8')?>">Weitere</a><?php } ?>
 </div>
